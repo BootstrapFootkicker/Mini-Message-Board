e22cdfbcbc32da74dc449ea20a3a35f01ab99a25
BootstrapFootkicker <ksealy93@gmail.com>
2024-08-01 22:40:51 -0400

    View Details
    Added ability to view individual message details
    Added cancel button to form
    Added back button to message detail

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
 const messages = [
-  {
-    text: "Hi there!",
-    user: " Amando",
-    added: new Date()
-  },
-    {
-    text: "Hello World!",
-    user: " Charles",
-    added: new Date()
-    },
-    ];
+  { text: "Hi there!", user: "Amando", added: new Date() },
+  { text: "Hello World!", user: "Charles", added: new Date() }
+];
 
-
-
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Mini-Messageboard', messages: messages});
+  res.render('index', { title: 'Mini-Messageboard', messages: messages });
 });
 
-
-router.get("/new", function(req, res, next){
-    res.render("form", {title: "New Page"});
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New Page' });
 });
-router.post("/new", function(req, res, next){
-    messages.push({text: req.body.message, user: req.body.user, added: new Date()});
-    res.redirect("/");
+
+router.post('/new', function(req, res, next) {
+  messages.push({ text: req.body.message, user: req.body.user, added: new Date() });
+  res.redirect('/');
+});
+
+router.get('/details/:id', function(req, res, next) {
+  const message = messages[req.params.id];
+  if (message) {
+    res.render('details', { title: 'Details', message: message });
+  } else {
+    res.status(404).send('Message not found');
+  }
 });
 
 module.exports = router;
